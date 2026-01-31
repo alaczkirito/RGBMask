f87428c6d46e39861a5af949f76b9a64668a684e
alaczkirito <cao.alacz@gmail.com>
2026-01-31 13:56:04 +0800

    Created player
    the wifi is too slow so moving back to laptop

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public enum playerMask
 {
@@ -12,8 +13,12 @@ public class GameManager : MonoBehaviour
 {
     #region Public Variables
     
+    [Header("Game Manager")]
+    public static GameManager Instance { get; private set; }
+    
     [Header("Player")] 
     public GameObject player;
+    public Rigidbody2D playerRB;
     public float CurrentHP;
     public float MaxHP;
     public float CurrentMana;
@@ -33,5 +38,43 @@ public class GameManager : MonoBehaviour
     public bool isAttacking = false;
     public bool canAttack = true;
     public float attackDamage;
+    
+    #endregion
 
+    #region Private Variables
+    
+    
+    
+    #endregion
+    
+    #region Setups
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.anyKey.isPressed) Debug.Log(playerDirection);
+        
+        if (Keyboard.current.aKey.isPressed) playerDirection.x = -1;
+        if (Keyboard.current.dKey.isPressed) playerDirection.x = 1;
+        if (Keyboard.current.wKey.isPressed) playerDirection.y = 1;
+        if (Keyboard.current.sKey.isPressed) playerDirection.y = -1;
+
+        
+        playerDirection.Normalize();
+        
+        playerRB.linearVelocity = playerDirection * moveSpeed;
+    }
+    
+    #endregion
 }
