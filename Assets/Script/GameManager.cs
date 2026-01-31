857ce1160528c40668489a0666eae1c45af22541
alacz <cao.alacz@gmail.com>
2026-01-31 12:57:39 +0800

    changes to game manager
    need to move to desktop

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,6 +10,8 @@ public enum playerMask
 
 public class GameManager : MonoBehaviour
 {
+    #region Public Variables
+    
     [Header("Player")] 
     public GameObject player;
     public float CurrentHP;
@@ -20,11 +22,16 @@ public class GameManager : MonoBehaviour
     public float MaxStamina;
     public bool canDash = true;
     public bool isDashing = false;
+    public bool longDash = false;
     public float dashForce;
+    public bool isRunning = false;
+    public bool canRun = false;
+    public float runSpeed;
     public float moveSpeed;
     public Vector2 playerDirection = new Vector2(0,0);
     public playerMask currentMask = playerMask.Green;
     public bool isAttacking = false;
-
+    public bool canAttack = true;
+    public float attackDamage;
 
 }
