6ff02d865c8442a937cfc6ae9db92073d082639b
alaczkirito <cao.alacz@gmail.com>
2026-04-01 23:54:24 +0800

    renew main branch

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Vector2 position;
+    //public Vector2 velocity;
+    public Vector2 direction;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        
+    }
+}
